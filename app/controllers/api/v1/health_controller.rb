1578c9fd371e39d32bf34b6e4b4a9f9fc55f9c6e
vladimir <v.khromov.work@gmail.com>
2025-09-15 16:01:19 +0300

    setup api namespace and health endpoint

---
+++ app/controllers/api/v1/health_controller.rb
@@ -0,0 +1,9 @@
+class Api::V1::HealthController < ApplicationController
+  def index
+    render json: {
+      status: 'ok',
+      timestamp: Time.current,
+      version: '1.0.0'
+    }
+  end
+end
