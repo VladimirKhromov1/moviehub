964935418ca007fa9dfde1348dbdbd2bfd39901b
vladimir <v.khromov.work@gmail.com>
2025-09-16 17:39:02 +0300

    setup secure authentication with HttpOnly cookies and configure cors

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -1,2 +1,25 @@
 class Api::V1::BaseController < ApplicationController
+  include ActionController::Cookies
+
+  skip_before_action :verify_authenticity_token
+
+  private
+
+  def set_auth_cookie(token)
+    cookies.signed[:auth_token] = {
+      value: token,
+      httponly: true,
+      secure: Rails.env.production?,
+      same_site: :lax,
+      expires: 24.hours.from_now
+    }
+  end
+
+  def current_user_token
+    cookies.signed[:auth_token]
+  end
+
+  def clear_auth_cookie
+    cookies.delete(:auth_token)
+  end
 end
