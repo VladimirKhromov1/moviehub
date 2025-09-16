2e4d6a15b387e79fa1c351248166b71a56813736
vladimir <v.khromov.work@gmail.com>
2025-09-16 17:40:11 +0300

    create User model with authentication

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  has_secure_password
+
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :password, presence: true, length: { minimum: 6 }, if: -> { new_record? || !password.nil? } #validates only if create or change password
+
+  before_save { self.email = email.downcase }
+end
