e5a1081fba043dade90621fc4d0bb942a4fd7f45
vladimir <v.khromov.work@gmail.com>
2025-09-25 19:13:54 +0300

    add pagination

--- Gemfile
+++ Gemfile
@@ -20,6 +20,8 @@ gem 'redis'
 gem 'sidekiq'
 gem 'sidekiq-cron'
 
+gem 'kaminari'
+
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem "tzinfo-data", platforms: %i[ windows jruby ]
 
--- app/controllers/api/v1/movies_controller.rb
+++ app/controllers/api/v1/movies_controller.rb
@@ -2,8 +2,26 @@ class Api::V1::MoviesController < ApplicationController
   before_action :set_movie, only: [:show]
 
   def index
-    movies = Movie.includes(:genres).order(created_at: :desc).limit(20)
-    render json: movies.map { |movie| movie_json(movie) }
+    page = params[:page]&.to_i || 1
+    per_page = params[:per_page]&.to_i || 20
+    per_page = [per_page, 100].min
+
+    movies = Movie.includes(:genres)
+                  .order(created_at: :desc)
+                  .page(page)
+                  .per(per_page)
+
+    render json: {
+      movies: movies.map { |movie| movie_json(movie) },
+      pagination: {
+        current_page: movies.current_page,
+        total_pages: movies.total_pages,
+        total_count: movies.total_count,
+        per_page: movies.limit_value,
+        has_next: !movies.last_page?,
+        has_prev: !movies.first_page?
+      }
+    }
   end
 
   def show
