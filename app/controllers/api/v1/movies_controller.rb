b2132a44ae9bf342bad35ac5dcc7afaed0a905cc
vladimir <v.khromov.work@gmail.com>
2025-09-28 23:55:26 +0300

    add actions for searching and filtering, refactoring

--- app/controllers/api/v1/movies_controller.rb
+++ app/controllers/api/v1/movies_controller.rb
@@ -1,34 +1,27 @@
 class Api::V1::MoviesController < Api::V1::BaseController
   before_action :set_movie, only: [:show]
 
-  def best_films
-    page = params[:page]&.to_i || 1
-    per_page = params[:per_page]&.to_i || 20
-    per_page = [per_page, 100].min
-
-    movies = Movie.best_films.includes(:genres)
-                  .page(page)
-                  .per(per_page)
+  def index
+    @movies = Movie.search_and_filter(search_params)
+                   .page(current_page)
+                   .per(per_page_limit)
 
     render json: {
-      movies: movies.map { |movie| movie_json(movie) },
-      pagination: pagination_json(movies)
+      movies: @movies.map { |movie| movie_json(movie) },
+      pagination: pagination_json(@movies),
+      filters: active_filters_json
     }
   end
 
-  def index
-    page = params[:page]&.to_i || 1
-    per_page = params[:per_page]&.to_i || 20
-    per_page = [per_page, 100].min
-
-    movies = Movie.includes(:genres)
-                  .order(created_at: :desc)
-                  .page(page)
-                  .per(per_page)
+  def best_films
+    @movies = Movie.best_films
+                   .with_genres_preloaded
+                   .page(current_page)
+                   .per(per_page_limit)
 
     render json: {
-      movies: movies.map { |movie| movie_json(movie) },
-      pagination: pagination_json(movies)
+      movies: @movies.map { |movie| movie_json(movie) },
+      pagination: pagination_json(@movies)
     }
   end
 
@@ -36,11 +29,56 @@ class Api::V1::MoviesController < Api::V1::BaseController
     render json: movie_json(@movie)
   end
 
+  def genres
+    @genres = Genre.joins(:movies)
+                   .select('genres.id, genres.name, COUNT(movies.id) as movies_count')
+                   .group('genres.id, genres.name')
+                   .order('genres.name')
+
+    render json: {
+      genres: @genres.map do |genre|
+        {
+          id: genre.id,
+          name: genre.name,
+          movies_count: genre.movies_count
+        }
+      end
+    }
+  end
+
+  def filter_stats
+    render json: Movie.filter_stats
+  end
 
   private
 
   def set_movie
     @movie = Movie.find(params[:id])
+  end
+
+  def search_params
+    params.permit(:search, :min_rating, :year, :sort_by).tap do |permitted|
+      if params[:genres].present?
+        genres = params[:genres].is_a?(String) ?
+                   params[:genres].split(',').map(&:strip) :
+                   Array(params[:genres])
+        permitted[:genres] = genres.reject(&:blank?)
+      end
+    end
+  end
+
+  def current_page
+    [params[:page].to_i, 1].max
+  end
+
+  def per_page_limit
+    per_page = params[:per_page].to_i
+    per_page = 20 if per_page <= 0
+    [per_page, 100].min
+  end
+
+  def active_filters_json
+    search_params.to_h.compact_blank
   end
 
   def movie_json(movie)
