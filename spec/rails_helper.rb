112b7631b18a01bdc4e9f22d0267b2dcc8a2038f
vladimir <v.khromov.work@gmail.com>
2025-09-29 23:12:50 +0300

    add rails helper

---
+++ spec/rails_helper.rb
@@ -0,0 +1,59 @@
+require 'spec_helper'
+ENV['RAILS_ENV'] ||= 'test'
+require_relative '../config/environment'
+# Prevent database truncation if the environment is production
+abort("The Rails environment is running in production mode!") if Rails.env.production?
+require 'rspec/rails'
+
+# Add additional requires below this line. Rails is not loaded until this point!
+require 'shoulda/matchers'
+require 'webmock/rspec'
+require 'database_cleaner/active_record'
+
+Rails.root.glob('spec/support/**/*.rb').sort_by(&:to_s).each { |f| require f }
+
+# Checks for pending migrations and applies them before tests are run.
+begin
+  ActiveRecord::Migration.maintain_test_schema!
+rescue ActiveRecord::PendingMigrationError => e
+  abort e.to_s.strip
+end
+RSpec.configure do |config|
+  # Disable transactional fixtures to use DatabaseCleaner
+  config.use_transactional_fixtures = false
+
+  # Automatically infer spec type from file location
+  config.infer_spec_type_from_file_location!
+
+  # Filter lines from Rails gems in backtraces
+  config.filter_rails_from_backtrace!
+
+  # Include FactoryBot syntax methods
+  config.include FactoryBot::Syntax::Methods
+
+  # Include Rails time helpers
+  config.include ActiveSupport::Testing::TimeHelpers
+
+  # Configure Shoulda Matchers
+  config.include(Shoulda::Matchers::ActiveModel, type: :model)
+  config.include(Shoulda::Matchers::ActiveRecord, type: :model)
+
+  # Configure DatabaseCleaner
+  config.before(:each) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+    travel_back
+  end
+end
+
+# Shoulda Matchers configuration
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
